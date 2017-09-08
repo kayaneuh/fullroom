bb520f929a1df4324eb6841ac6a3a4293cb4e652
Kaliane Net <n.kaliane@gmail.com>
2017-09-08 15:48:27 +0000

    upload photo de profil

--- Gemfile
+++ Gemfile
@@ -31,6 +31,9 @@ gem 'devise'
 # devise traduction
 gem 'devise-i18n'
 
+# pour les images
+gem 'paperclip'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908145017) do
+ActiveRecord::Schema.define(version: 20170908154049) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 20170908145017) do
     t.datetime "updated_at",                          null: false
     t.string   "fullname"
     t.string   "school"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
